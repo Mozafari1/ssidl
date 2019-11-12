c522291407d1ae1341854eee8a81843225df72d3
Mozafari1 <m_rahmat@hotmail.no>
2019-11-12 23:28:19 +0100

    petals

--- c++/q_2.cpp
+++ c++/q_2.cpp
@@ -3,5 +3,33 @@
 using namespace std;
 
 std::string hmilu(int petals){
-    
+    int new_petals = petals %6;
+    switch (new_petals)
+    {
+    case 0 /* constant-expression */:
+        /* code */
+        return "Not at all";
+    case 1:
+        return "I love you";
+    case 2:
+        return "A little";
+    case 3:
+        return "A lot";
+    case 4: 
+        return "Passionately";
+    case 5:
+        return "Madly";
+
+    default:
+        break;
+    }
+}
+
+int main(){
+    int petals;
+    std::cout<<"Please enter a value to get know how much you love him/her: ";
+    std::cin>>petals;
+    string HMILU = hmilu(petals);
+    std::cout<<"The result is: "<< HMILU;
+
 }
